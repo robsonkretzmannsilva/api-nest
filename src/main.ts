1f99a9e22b55ad7ee075ed8daaba6ba323507849
Robson Kretzmann da Silva <robsonkretzmann@hotmail.com>
2024-10-25 12:18:11 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // ou a origem específica que você deseja permitir
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
   const config = new DocumentBuilder()
-    .setTitle('API com NestJS')
-    .setDescription('Documentação da API')
+    .setTitle('API Docs')
+    .setDescription('API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
-  await app.listen(8080);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document); // URL: /swagger
+    await app.listen(process.env.PORT || 3000); // Ensure correct port is used
 }
 bootstrap();
